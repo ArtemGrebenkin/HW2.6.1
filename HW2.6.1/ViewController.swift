ad67981bcc408133dc0815c92dc7e39780196112
Artem Grebenkin <artoisgrebenkin@gmail.com>
2019-10-05 20:30:07 +0600

    Initial Commit

---
+++ HW2.6.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.6.1
+//
+//  Created by Artem Grebenkin on 10/5/19.
+//  Copyright © 2019 Artem Grebenkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
